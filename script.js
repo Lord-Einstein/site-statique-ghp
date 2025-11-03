28eb683bb3c3ecc85784174402ddf437598c621b
Lord-Einstein <darylleinstein@gmail.com>
2025-11-03 14:45:38 +0100

    First Commit.

---
+++ script.js
@@ -0,0 +1,10 @@
+document.body.style.backgroundColor = 'cyan';
+
+document.getElementById('Color1').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'gray';
+})
+
+document.getElementById('Color2').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'red';
+})
+
